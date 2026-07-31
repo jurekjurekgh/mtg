eb34c7cac2dd8340be33bfa904f929e943003241
jurekjurekgh <311390603+jurekjurekgh@users.noreply.github.com>
2026-07-31 21:51:43 +0000

    Cover automatic turn resource reset
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/protocol/types.js
+++ src/protocol/types.js
@@ -4,7 +4,7 @@
 /** @typedef {{ playerId: string, turn: object, zones: object, legalCommands: Command[] }} PlayerView */
 
 export const COMMAND_TYPES = Object.freeze(['pass_priority', 'move_object', 'draw_card', 'concede']);
-export const EVENT_TYPES = Object.freeze(['game_created', 'object_moved', 'card_drawn', 'damage_dealt', 'life_changed', 'mana_changed', 'land_played', 'attackers_declared', 'blockers_declared', 'damage_marked', 'creature_destroyed', 'object_tapped', 'object_untapped', 'player_lost', 'priority_passed', 'step_advanced', 'player_conceded', 'command_rejected']);
+export const EVENT_TYPES = Object.freeze(['game_created', 'object_moved', 'card_drawn', 'damage_dealt', 'life_changed', 'mana_changed', 'land_played', 'attackers_declared', 'blockers_declared', 'damage_marked', 'creature_destroyed', 'object_tapped', 'object_untapped', 'turn_started', 'player_lost', 'priority_passed', 'step_advanced', 'player_conceded', 'command_rejected']);
 
 export function command(type, playerId, data = {}) {
   if (!COMMAND_TYPES.includes(type) || !playerId) throw new TypeError('Nieprawidłowa komenda');
